450a36384b28ab02b3740e9e91bf46213a663e8c
azbury <acasbury25@aol.com>
2020-05-23 01:32:50 -0400

    adding javascript for new user form

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,25 @@
 const BASE_URL = "http://localhost:3000"
 const USERS_URL = `${BASE_URL}/users`
+
+document.addEventListener("DOMContentLoaded", () => {
+    const userFormContainer = document.querySelector(".container")
+    userFormContainer.addEventListener('submit', e => {
+        e.preventDefault()
+        newUser(e.target)
+    })
+})
+
+function newUser(userDate) {
+    let formData = {
+        "username": userDate.username.value
+    }
+
+    let configObj = {
+        method: "Post",
+        headers: {
+            "content-type": "application/json",
+            "accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+}
