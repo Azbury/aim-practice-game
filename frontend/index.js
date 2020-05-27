f7317081a806fc47b5bae9db0b226f6bc0ef10f1
azbury <acasbury25@aol.com>
2020-05-27 14:50:10 -0400

    adding functionality for new players

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,18 @@
 const BASE_URL = "http://localhost:3000"
 const USERS_URL = `${BASE_URL}/users`
 const userFormContainer = document.querySelector(".container")
+const newPlayer = document.getElementById("new-player")
 
 document.addEventListener("DOMContentLoaded", () => {
     userFormContainer.addEventListener('submit', function(e) {
         e.preventDefault()
         newUser(e.target)
+    })
+    newPlayer.addEventListener('click', function(e) {
+        e.preventDefault()
+        userFormContainer.style.display = "block"
+        newPlayer.style.display = "none"
+        document.getElementById("user").remove()
     })
 })
 
@@ -25,9 +32,11 @@ function newUser(userDate) {
 
     fetch(USERS_URL, configObj).then(function(reponse) {return reponse.json()}).then(function(user) {
         userFormContainer.style.display = "none"
+        newPlayer.style.display = "block"
         let newUser = document.createElement('h2')
+        newUser.setAttribute('id', 'user')
         newUser.innerHTML = `Username: ${user.username}`
-        document.getElementById("username").append(newUser)
+        document.getElementById("username").appendChild(newUser)
     }).catch(function(error) {
         document.body.innerHTML = error.message
     });
