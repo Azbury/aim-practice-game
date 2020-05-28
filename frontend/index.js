f79479363754ede6735c5a0265ee2ca6917f16b5
azbury <acasbury25@aol.com>
2020-05-27 21:38:46 -0400

    adding enemy

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,8 @@ const BASE_URL = "http://localhost:3000"
 const USERS_URL = `${BASE_URL}/users`
 const userFormContainer = document.querySelector(".container")
 const newPlayer = document.getElementById("new-player")
+const creeper = "http://www.pngmart.com/files/7/Minecraft-PNG-Clipart.png"
+const myCanvas = document.getElementById("canvas")
 
 document.addEventListener("DOMContentLoaded", () => {
     userFormContainer.addEventListener('submit', function(e) {
@@ -13,6 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
         userFormContainer.style.display = "block"
         newPlayer.style.display = "none"
         document.getElementById("user").remove()
+        addCreeper()
     })
 })
 
@@ -37,7 +40,6 @@ function newUser(userDate) {
         newUser.setAttribute('id', 'user')
         newUser.innerHTML = `Username: ${user.username}`
         document.getElementById("username").appendChild(newUser)
-        getScores(user.id)
     }).catch(function(error) {
         document.body.innerHTML = error.message
     });
@@ -47,4 +49,10 @@ function getScores(userID) {
     fetch(`${USERS_URL}/${userID}`).then(function(reponse) {return reponse.json()}).then(function(user) {
         
     })
+}
+
+function addCreeper() {
+    let creeperImg = document.createElement('img')
+    creeperImg.src = creeper
+    myCanvas.drawImage(creeperImg, 100, 100, 50, 50)
 }
