fb185c5de7330198c783b5d479ef455c44c35b8b
azbury <acasbury25@aol.com>
2020-05-29 14:11:15 -0400

    can click on creeper to kill them

--- frontend/index.js
+++ frontend/index.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     userFormContainer.addEventListener('submit', function(e) {
         e.preventDefault()
         newUser(e.target)
-        setInterval(addCreeper, 3000)
+        setInterval(addCreeper, 1000)
     })
     newPlayer.addEventListener('click', function(e) {
         e.preventDefault()
@@ -41,6 +41,7 @@ function newUser(userDate) {
         newUser.innerHTML = `Username: ${user.username}`
         let newScore = document.createElement('h3')
         newScore.innerHTML = "Score: 0"
+        newScore.setAttribute('id', 'current-score')
         document.getElementById("scores").appendChild(newScore)
         document.getElementById("username").appendChild(newUser)
     }).catch(function(error) {
@@ -55,14 +56,20 @@ function getScores(userID) {
 }
 
 function addCreeper() {
+    
     let newCreeper = document.createElement('img')
     newCreeper.src = creeper
     newCreeper.style = "width:50px;height:50px"
     newCreeper.style.position = "absolute"
     newCreeper.style.top = `${getRandomArbitrary(0,480)}px`
     newCreeper.style.left = `${getRandomArbitrary(0,480)}px`
+    newCreeper.setAttribute('id', 'creeper')
     newCreeper.addEventListener('click', function(e) {
         e.target.remove()
+        const currentScore = document.getElementById("current-score")
+        scoreArray = currentScore.innerHTML.split(" ")
+        scoreArray[1] = parseInt(scoreArray[1]) + 1
+        currentScore.innerHTML = scoreArray.join(" ")
     })
     gameBoard.appendChild(newCreeper)
 }
